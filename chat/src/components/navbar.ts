a67581adf438f9b33bed4f1ffecdafdb6d89c5a1
ayoub-abn <ayoub111abn@gmail.com>
2025-10-12 23:19:03 +0100

    ya bismi lah

---
+++ chat/src/components/navbar.ts
@@ -0,0 +1,17 @@
+export function renderNavBar():string
+{
+    return ` <nav id="nav-bar" class="flex flex-row justify-around items-center" >
+                <div class="basis-15">
+                    <img  class="w-15 h-15 " src="../img/canvas.png" alt="logo">
+                </div>
+                <div class="basis-1/3 flex items-center">
+                    <input class="w-[400px]  h-8 border-1 border-white rounded-s-lg text-white border-r-0" type="text" placeholder="Search">
+                    <button class="w-[40px] h-8 border-1 border-white rounded-tr-lg rounded-br-lg text-white border-l-0 "> <span class="material-symbols-outlined h-6">search</span></button>
+                </div>
+                <div class="basis-15" >
+                    <button class="w-10 h-10 rounded-full border-white border-1  bg-black hover:bg-[linear-gradient(to_right,#D73B3F,#FD1D1D,#711F21)] ">
+                        <span class="material-symbols-outlined color text-white">notifications</span>
+                    </button>
+                </div> 
+            </nav>`
+};
